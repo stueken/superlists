53dec78b5640efa35ce052b37168810e332c23db
Norbert Stueken <norbert.stueken@gmail.com>
2015-07-31 11:12:03 +0200

    test & feat: Adding of one list item works

--- lists/tests.py
+++ lists/tests.py
@@ -1,7 +1,7 @@
-from django.template.loader import render_to_string
 from django.core.urlresolvers import resolve
-from django.test import TestCase
 from django.http import HttpRequest
+from django.template.loader import render_to_string
+from django.test import TestCase
 
 from lists.views import home_page
 
@@ -16,4 +16,21 @@ class HomePageTest(TestCase):
         request = HttpRequest()
         response = home_page(request)
         expected_html = render_to_string('home.html')
+        self.assertEqual(response.content.decode(), expected_html)
+
+    def test_home_page_can_save_a_POST_request(self):
+        # **Arrange** all necessary preconditions and inputs.
+        request = HttpRequest()
+        request.method = 'POST'
+        request.POST['item_text'] = 'A new list item'
+
+        # **Act** on the object or method under test.
+        response = home_page(request)
+
+        # **Assert** that the expected results have occured
+        self.assertIn('A new list item', response.content.decode())
+        expected_html = render_to_string(
+            'home.html',
+            {'new_item_text': 'A new list item'}
+        )
         self.assertEqual(response.content.decode(), expected_html)
