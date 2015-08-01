c3de96e7a050ae78913aa5cfc6cbd2f290d8b5be
Norbert Stueken <norbert.stueken@gmail.com>
2015-08-01 11:38:18 +0200

    refactor: Changes to tests, URLs, views and templates.

--- superlists/urls.py
+++ superlists/urls.py
@@ -7,6 +7,7 @@ urlpatterns = [
     # url(r'^$', 'superlists.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', views.home_page, name='home'),
+    url(r'^lists/new$', views.new_list, name='new_list'),
     url(r'^lists/the-only-list-in-the-world/$', views.view_list, name='view_list')
     # url(r'^admin/', include(admin.site.urls)),
 ]
