cffd6ddd937ebf138657884a5fdd1aacf7141802
Norbert Stueken <norbert.stueken@gmail.com>
2015-08-29 19:07:44 +0200

    feat: login server-side (part 1)

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import include, url
 from lists import views as list_views
 from lists import urls as list_urls
+from accounts import urls as accounts_urls
 
 urlpatterns = [
     # Examples:
@@ -8,5 +9,6 @@ urlpatterns = [
     # url(r'^blog/', include('blog.urls')),
     url(r'^$', list_views.home_page, name='home'),
     url(r'^lists/', include(list_urls)),
+    url(r'^accounts/', include(accounts_urls))
     # url(r'^admin/', include(admin.site.urls)),
 ]
