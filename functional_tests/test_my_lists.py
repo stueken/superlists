44d9e0c010f9edc7ed7676d56f090ab2a8ab7872
Norbert Stueken <norbert.stueken@gmail.com>
2015-09-12 13:32:10 +0200

    fix: Wait until link to My lists is visible

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -14,7 +14,9 @@ class MyListsTest(FunctionalTest):
         first_list_url = self.browser.current_url
 
         # She notices a "My lists" link, for the first time.
-        self.browser.find_element_by_link_text('My lists').click()
+        self.wait_for(
+            lambda: self.browser.find_element_by_link_text('My lists').click()
+        )
 
         # She sees that her list is in there, named according to its
         # first list item
